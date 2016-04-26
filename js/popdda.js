1fc541645b1b5e05d49a0ffc117e648b5faae0c9
Marco Pasi <mfpasi@gmail.com>
2016-04-26 23:04:35 +0100

    Added colormap selection; rationalised namespacing, still requires work

--- js/popdda.js
+++ js/popdda.js
@@ -16,8 +16,16 @@ var dinucl = "GG GA AG AA GC GT AT AC CA TA TG CG CC CT TC TT".split(" ");
 var dinucli = "GG GA AG AA GC GT AT TA TG CG".split(" ");
 var skippers= {
     inner: [4,6,7,9],
-    outer: [8,9,13,14,15,16]},
-    unitH = {
+    outer: [7,8,12,13,14,15]},
+    precision = {               // precision of output
+        shift: 2,
+        slide: 2,
+        rise:  2,
+        tilt:  1,
+        roll:  1,
+        twist: 1 
+    },
+    unitH = {                   // unit
         shift: '\u212B',
         slide: '\u212B',
         rise: '\u212B',
@@ -25,14 +33,23 @@ var skippers= {
         roll: '\u00B0',
         twist: '\u00B0'
     },
-    vrange = {
+    vrange = {                  // view range
         shift: [-1.0, 1.0],
         slide: [-1.5, 0.5],
         rise:  [+2.6, 3.8],
         tilt:  [-5.0, 5.0],
         roll:  [-5.0, 15],
         twist: [+20, 40]
+    },
+    colormaps = {
+        jet8:   ["#000080","#0010ff","#00a4ff","#40ffb7","#b7ff40","#ffb900","#ff3000","#800000"],
+        RdBu9:  ['#b2182b','#d6604d','#f4a582','#fddbc7','#f7f7f7','#d1e5f0','#92c5de','#4393c3','#2166ac'],
+        BrBG10: ['#543005','#8c510a','#bf812d','#dfc27d','#f6e8c3','#c7eae5','#80cdc1','#35978f','#01665e','#003c30'],
+        Spec11: ['#9e0142','#d53e4f','#f46d43','#fdae61','#fee08b','#ffffbf','#e6f598','#abdda4','#66c2a5','#3288bd','#5e4fa2']
     };
+colormaps.RdBu9.reverse();
+colormaps.BrBG10.reverse();
+colormaps.Spec11.reverse();
 
 var margin = {top: 15, right: 100, bottom: 20, left: 70},
     gridSize = 20,
@@ -40,10 +57,8 @@ var margin = {top: 15, right: 100, bottom: 20, left: 70},
     cbGridSize = 15,
     width = (gridSize+gridPad)*10,
     height = (gridSize+gridPad)*16,
-    flexscales = 10,
     transitionDuration = 1000, // ms
-    // rainbow. XXX use perceptually uniform scale
-    colors = ["#000080", "#0010ff", "#00a4ff", "#40ffb7", "#b7ff40", "#ffb900", "#ff3000", "#800000"];
+    svg, cursorInfoCallback, vname = "twist", cname = "jet8";
 
 function xScale(x) {
     return +x*(gridSize+gridPad);
@@ -67,15 +82,20 @@ function unit(variable) {
     return unitH[variable]
 }
 
+// XXX Use the function.call(instance) pattern
+//     to wrap all this into an object with an
+//     assigned id, tsv; then the _this pattern
+//     can be used for callbacks.
 function popdda(id, tsv) {
-    this.svg = d3.select(id).append("svg")
-        .attr("width", width + margin.left + margin.right)
-        .attr("height", height + margin.top + margin.bottom)
+    svg = d3.select(id).append("svg")
+        .attr("width",  width  + margin.left + margin.right)
+        .attr("height", height + margin.top  + margin.bottom)
         .append("g")
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
+    var xlabelsg = svg.append("g").attr("class", "xlabels");
     /* X labels */
-    this.xlabels = this.svg.selectAll(".xlabel")
+    var xlabels = xlabelsg.selectAll(".xlabel")
         .data(dinucli)
       .enter().append("text")
         .text(function (d) { return d; })
@@ -85,8 +105,9 @@ function popdda(id, tsv) {
         .attr("transform", "translate(-2,12)")
         .attr("class", "x label");
 
+    var ylabelsg = svg.append("g").attr("class", "ylabels");
     /* Y labels */
-    this.ylabels = this.svg.selectAll(".ylabel")
+    var ylabels = ylabelsg.selectAll(".ylabel")
         .data(dinucl)
       .enter().append("text")
         .text(function(d) { return d[0]+".."+d[1]; })
@@ -96,73 +117,82 @@ function popdda(id, tsv) {
         .attr("transform", "translate(-20,3)")
         .attr("class", "y label");
 
-    this.colorby = function(vname, domain) {
+    this.update = function(namev, domain, namec) {
         /* 
          * Color the heatmap by the stored value "vname",
          * using values in the range specified by domain.
          */
-
+        if(namev)
+            vname = namev;
+        if(namec)
+            cname = namec;
+        var colors= colormaps[cname];
         var domain = (domain ? domain : vrange[vname]);
-        
-        // Add a label to the colorbar
-        var cblabel = $(".cblabel");
-        if(cblabel.length == 0) { // enter!
-            svg.append("text")
-                .attr("class", "cblabel")
-                .attr("x", width + gridSize)
-                .attr("y", 0)
-                .style("text-anchor", "top");
-            cblabel = $(".cblabel");
-        }
-        // update!
-        cblabel.text(capitalize(vname)+" ("+unit(vname)+")");
-        
-        var hlabel = cblabel[0].getBBox().height;
-
         // Update colorScale
         var colorScale = d3.scale.quantile()
             .domain(domain)
             .range(colors);
-
-        // Bind data to <g> containers and children
         var data = q2cb(colorScale.quantiles(), domain);
-        var colorbar = _this.svg.selectAll(".cb").data(data);
-        colorbar.select(".cbrect").data(data);
-        colorbar.select(".cbtick").data(data);
-        colorbar.exit().remove();
-        
-        // Deal with new elements
-        var cbenter = colorbar.enter().append("g")
-            .attr("class", "cb");
-        cbenter.append("rect")
-            .attr("class", "cbrect")
-            .attr("x", width + gridSize)
-            .attr("y", function(d, i) { return hlabel + cbGridSize * i; })
-            .attr("width", cbGridSize)
-            .attr("height", cbGridSize);
-        cbenter.append("text")
-            .attr("class", "cbtick")
-            .attr("x", width + gridSize + 1.3*cbGridSize)
-            .attr("y", function(d, i) { return hlabel + cbGridSize * i; })
-            .style("text-anchor", "start")
-            .attr("transform", "translate(0, 4)");
+        var data2= data.concat(domain[domain.length - 1]);
 
-        // Update
-        _this.svg.selectAll(".cbrect").transition().duration(transitionDuration)
-            .style("fill", function(d, i) { return colors[i]; });
-        _this.svg.selectAll(".cbtick")
+        // Update the colorbar label
+        var colorbar = svg.select(".colorbar");
+        var cblabel = colorbar.select(".cblabel");
+        cblabel
             .transition().duration(transitionDuration/2)
             .style("opacity", 0)
             .transition().duration(transitionDuration/2)
             .style("opacity", 1)
-            .text(function(d) {return d.toFixed(1);});
-        _this.abc.transition().duration(transitionDuration)
-            .style("fill", function(d) { return colorScale(+d[vname]); });
+            .text(capitalize(vname)+" ("+unit(vname)+")");
 
-        // Update cursor text function
-        _this.cursorText = function(d) {
-            return d.tetrad+":"+d[vname]+unit(vname);
+        // Move the colorbar accordingly
+        var cb = svg.select(".cb");
+        cb.attr("transform", "translate(0, " +
+                (cblabel[0][0].getBBox().height) + ")");
+        
+        // Bind data to rects and texts
+        var cbrects = cb.selectAll(".cbrect").data(data);
+        var cbticks = cb.selectAll(".cbtick").data(data2);
+        cbrects.exit()
+            .transition().duration(transitionDuration)
+            .style("opacity", 0)
+            .remove();
+        cbticks.exit()
+            .transition().duration(transitionDuration/2)
+            .style("opacity", 0)
+            .remove();
+        
+        // Deal with new elements
+        cbrects.enter().append("rect")
+            .attr("class", "cbrect")
+            .attr("y", function(d, i) { return cbGridSize * i; })
+            .attr("width", cbGridSize)
+            .attr("height", cbGridSize);
+        cbticks.enter().append("text")
+            .attr("class", "cbtick")
+            .attr("x", cbGridSize + 10)
+            .attr("y", function(d, i) { return cbGridSize * i; })
+            .style("text-anchor", "start")
+            .attr("transform", "translate(0, 4)");
+
+        // Update
+        cbrects.transition().duration(transitionDuration)
+            .style("opacity", 1)
+            .style("fill", function(d, i) { return colors[i]; });
+        cbticks
+            .transition().duration(transitionDuration/2)
+            .style("opacity", 0)
+            .transition().duration(transitionDuration/2)
+            .style("opacity", 1)
+            .text(function(d) {return d.toFixed(precision[vname]);});
+        svg.selectAll("rect.cell").transition().duration(transitionDuration)
+            .style("fill", function(d) { return colorScale(+d[vname]); });
+        
+        // Update cursor info
+        cursorInfoCallback = function(d) {
+            return d[vname] + unit(vname);
         };
+
         return true;
     };
     
@@ -182,7 +212,7 @@ function popdda(id, tsv) {
                print "%4s %4s %2d %2d %2d"%(wtet, abc, j+1, wi[0], wi[1])
                print "%4s %4s %2d %2d %2d"%(ctet, abc, 36-j, ci[0], ci[1])
 *****/
-    var _this = this;
+    
     d3.tsv(
         tsv,
         // (ab)Use accessor to filter data on input
@@ -194,19 +224,38 @@ function popdda(id, tsv) {
         },
         // Build the UI
         function(error, data) {
-            _this.cursor = _this.svg.append("text")
-                .attr("x", width + 0.5*gridSize)
-                .attr("y", 0.5*height)
-                .attr("class", "tcursor")
-                .attr("width", 30)
-                .attr("height", 20);
-            _this.cursorText = function(d) {return d.tetrad;};
-            // XXX encapsulate text in <g.tcursor> which handles opacity (here)
-            // XXX bind d to the text and handle update in mousevoer (here)
-            // XXX provide an info box to be bound subsequently (e.g. colorby)
-            //     (or select <g.tcursor>s & append directly in "colorby")
+            // Add a g for the colorbar
+            var cb = svg.append("g")
+                .attr("class", "colorbar")
+                .attr("transform", "translate("+ (width + gridSize)
+                      +",0)");
+            // a sub-g the cb label
+            cb.append("text")
+                .attr("class", "cblabel")
+                .style("text-anchor", "top")
+                .text("\u00a0");
+            // and a sub-g the actual cb
+            cb.append("g")
+                .attr("class", "cb");
             
-            _this.abc = _this.svg.selectAll("rect.cell")
+            // Add a cursor information box, which disappears onmouseout
+            var cursor = svg.append("g")
+                .attr("class", "tcursor")
+                .attr("transform", "translate("+
+                      (width + gridSize)+","+
+                      (height- 17)+")");
+            // Add a tetrad text, updated here 
+            var cursorText = cursor.append("text")
+                .attr("class","tc-tetrad");
+            // Add an info text, with a callback to be used by methods
+            var cursorInfo = cursor.append("text")
+                .attr("transform", "translate(0, 14)")
+                .attr("class","tc-info");
+            cursorInfoCallback = function(d) { return ""; }
+
+            // Create the heatmap
+            var heatmap = svg.append("g").attr("class", "heatmap");
+            abc = heatmap.selectAll("rect.cell")
                 .data(data)
               .enter().append("rect")
                 .attr("x", function(d) { return xScale(d.inner); })
@@ -219,23 +268,24 @@ function popdda(id, tsv) {
                 .style("fill", "white")
                 .style("cursor", "hand")
                 .on("mouseover", function (d) {
-                    _this.cursor
+                    cursor
                         .transition().duration(transitionDuration/10)
-                        .style("opacity", 1)
-                        .text(_this.cursorText(d));
+                        .style("opacity", 1);
+                    cursorText.text(d.tetrad);
+                    cursorInfo.text(cursorInfoCallback(d));
                 })
                 .on("mouseout", function (d) {
-                    _this.cursor
+                    cursor
                         .transition().duration(transitionDuration/2)
                         .style("opacity", 0);
                 })
                 .on("click", function (d) {
-                    _this.svg.selectAll("rect.cell").classed("selected",false);
+                    svg.selectAll("rect.cell").classed("selected",false);
                     d3.select(this).classed("selected",true);
                     loadStiffnessPage(d.tetrad, d.oligo, d.pos);
                 });
-            
-            _this.colorby("twist");
+
+            update("twist", null, "jet8");
         });
     return this;
 }
@@ -252,6 +302,6 @@ function loadStiffnessPage(tetrad, oligomer, position) {
      *             of the oligo; values > 18 are for the Crick strand.
      *
      */
-    $("#stiffness").text(
+    $("#stiffness-external").text(
         tetrad+":"+oligomer+"@"+position);
 }
